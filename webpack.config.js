42b85e2f7aeded0f868e3d01d41d4cccc51a9ff5
Max Rahder <rahder@gmail.com>
2018-10-15 18:31:15 -0500

    Updated starter to be more like the sencha ext react starter

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
-import path from "path";
-import portfinder from "portfinder";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtWebpackPlugin = require("@sencha/ext-react-webpack-plugin");
+const portfinder = require("portfinder");
+const sourcePath = path.join(__dirname, "./src");
 
-module.exports = env => {
+module.exports = function(env) {
+  env = env || {};
+  var buildprofile =
+    env.profile || process.env.npm_package_extbuild_defaultprofile;
+  var buildenvironment =
+    env.environment || process.env.npm_package_extbuild_defaultenvironment;
+  var buildverbose =
+    env.verbose || process.env.npm_package_extbuild_defaultverbose;
+  if (buildprofile == "all") {
+    buildprofile = "";
+  }
+  const isProd = buildenvironment === "production";
+
+  portfinder.basePort = (env && env.port) || 1962; // the default port to use
   return portfinder.getPortPromise().then(port => {
-    console.log("++");
-    console.log(__dirname);
-    console.log(port);
+    const plugins = [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+        hash: true
+      })
+      // new ExtWebpackPlugin({
+      //   framework: "react",
+      //   port: port,
+      //   profile: buildprofile,
+      //   environment: buildenvironment,
+      //   verbose: buildverbose
+      // })
+    ];
+    if (!isProd) {
+      plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
     return {
+      mode: "development",
+      cache: true,
+      devtool: isProd ? "source-map" : "cheap-module-source-map",
+      context: sourcePath,
       entry: path.join(__dirname, "src", "index.js"),
       output: {
         path: path.join(__dirname, "build"),
@@ -17,16 +50,11 @@ module.exports = env => {
       resolve: {
         modules: [path.resolve(__dirname, "src"), "node_modules"]
       },
-      devServer: {
-        contentBase: path.join(__dirname, "src")
-      },
+
       module: {
         rules: [
           {
-            // this is so that we can compile any React,
-            // ES6 and above into normal ES5 syntax
             test: /\.(js|jsx)$/,
-            // we do not want anything from node_modules to be compiled
             exclude: /node_modules/,
             use: ["babel-loader"]
           },
@@ -44,11 +72,31 @@ module.exports = env => {
           }
         ]
       },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: path.join(__dirname, "src", "index.html")
-        })
-      ]
+      plugins,
+      devServer: {
+        contentBase: path.join(__dirname, "src", "./build"),
+        historyApiFallback: true,
+        hot: false,
+        host: "0.0.0.0",
+        port: port,
+        disableHostCheck: false,
+        compress: isProd,
+        inline: !isProd,
+        stats: {
+          assets: false,
+          children: false,
+          chunks: false,
+          hash: false,
+          modules: false,
+          publicPath: false,
+          timings: false,
+          version: false,
+          warnings: false,
+          colors: {
+            green: "\u001b[32m"
+          }
+        }
+      }
     };
   });
 };
